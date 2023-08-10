3c69193b91245c70c8d2f4644ac241d6f8b09700
Vivek Kumar <vickyvivek557@gmail.com>
2023-08-10 13:17:33 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String []args){
+        B objOfB = new B();
+        String s = objOfB.meth();
+    }
 }
